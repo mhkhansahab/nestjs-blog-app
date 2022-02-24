50f5027e9af93c07875bfaa8320f223aca4d9cbb
mhkhansahab <hamza.khansahab@gmail.com>
2022-02-25 01:45:43 +0500

    fixed env variable bug

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from "src/user/user.module";
 import { AuthController } from "./auth.controller.js";
 import { AuthService } from "./auth.service.js";
 
 @Module({
-    imports: [UserModule, JwtModule.register({
+    imports: [UserModule,  ConfigModule.forRoot(), JwtModule.register({
         secret: process.env.JWT_SECRET,
         signOptions: { expiresIn: '3600s' },
     })],
